623d964b9e5d96d1ae1b0b6556103ffa237ba621
Pitisha96 <petrikin1996@gmail.com>
2020-12-27 14:50:47 +0300

    added test for CreateClientAction

---
+++ src/test/java/com/pitisha96/actions/CreateClientActionTest.java
@@ -0,0 +1,26 @@
+package com.pitisha96.actions;
+
+import com.pitisha96.Clinic;
+import com.pitisha96.IClinic;
+import com.pitisha96.Validator;
+import com.pitisha96.clients.Client;
+import org.junit.jupiter.api.Test;
+import java.io.StringReader;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateClientActionTest {
+
+    @Test
+    void execute() {
+        CreateClientAction test=new CreateClientAction();
+        Validator validator=new Validator(new Scanner(new StringReader("Илья")));
+        IClinic clinic = new Clinic();
+        test.execute(clinic,validator);
+        Client expected=new Client("Илья");
+        List<Client> actual=clinic.searchClient("Илья");
+        assertTrue(actual.size()==1 && actual.contains(expected));
+    }
+}
