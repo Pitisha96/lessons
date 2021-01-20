9532dff111f7e785907303400a0a4613cc0aad25
Pitisha96 <petrikin1996@gmail.com>
2021-01-20 15:07:42 +0300

    added Test for Validator

---
+++ src/test/java/com/pitisha96/ValidatorTest.java
@@ -0,0 +1,34 @@
+package com.pitisha96;
+
+import com.pitisha96.actions.Action;
+import org.junit.jupiter.api.Test;
+
+import java.io.StringReader;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorTest {
+    @Test
+    public void whenValidatorShouldThrowsUnsupportedOperationException()
+    throws UnsupportedOperationException{
+        Set<Integer> set=new LinkedHashSet<>();
+        set.add(1);
+        assertThrows(UnsupportedOperationException.class,()->
+            new Validator(new Scanner(new StringReader("2")))
+                    .getIntFromList("",set)
+        );
+    }
+
+    @Test
+    public void whenValidatorShouldThrowsIllegalArgumentException(){
+        Set<Integer> set=new LinkedHashSet<>();
+        set.add(1);
+        assertThrows(IllegalArgumentException.class,()->
+           new Validator(new Scanner(new StringReader("h")))
+                   .getIntFromList("",set)
+        );
+
+    }
+
+}
